fcc433cb759e8c19c656f5d3308769002073ddb2
Rus-ky <kylecledoro@gmail.com>
2026-04-20 21:43:08 +0800

    Forda activities

---
+++ Task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+    public static void main(String[] args) {
+        char letter = 'H';        
+        byte b = 3;               
+        short s = 1;              
+        int i = 10;               
+        float f = 2.0f;           
+        boolean bool = true;      
+
+        String result = "" + letter + b + s + s + i + " w" + 0 + "rld " + f + " " + bool;
+
+        System.out.println(result);
+    }   
+}
